f8bf98031b6202b64be897e5886d88a1fd7745f5
Nathaly Gomez Lopez <nath.gomez.lopez@gmail.com>
2023-08-29 16:46:02 +0200

    searchbar sur home : ok

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -2,6 +2,14 @@ class BarsController < ApplicationController
 
   def index
     @bars = Bar.all
+
+    if params[:query].present?
+      @bars = @bars.where('name ILIKE ?', "%#{params[:query]}%")
+    end
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "bars/index", locals: {bars: @bars}, formats: [:html] }
+    end
   end
 
   def show
