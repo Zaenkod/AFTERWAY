e4feb6097dcc0f75f24e1bf451a24db3433a5bea
Nathaly Gomez Lopez <nath.gomez.lopez@gmail.com>
2023-09-04 14:58:08 +0200

    Searchbar : ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,29 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    if user_signed_in?
-      @host_events = current_user.events
-      @invitation_events = current_user.participations
+    @host_events = current_user.events
+    @invitation_events = current_user.participations
+  end
+
+  def recap
+    @host_events = current_user.events
+    @invitation_events = current_user.participations
+
+    if params[:query].present?
+      @host_events = @host_events.where("title ILIKE ?", "%#{params[:query]}%")
+      @invitation_events = @invitation_events.where("title ILIKE ?", "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text {
+        render partial: "recap",
+        locals: {
+          host_events: @host_events,
+          invitation_events: @invitation_events
+        },
+        formats: [:html]
+      }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   end
   resources :votes, only: %i[destroy]
   get "/myevents", to: "events#myevents", as: "myevents"
-
+  get "/recap", to: "pages#recap", as: "recap"
 end
