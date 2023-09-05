90cb685e06c476cfa3966dc987b1c4817e6c4e1b
michel tran <tran.michel1@gmail.com>
2023-09-05 13:01:16 +0200

    navbar map

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableElement", "togglableCard"]
+  static targets = ["togglableElement", "togglableCard","map","list"]
 
   connect() {
   }
@@ -10,5 +10,7 @@ export default class extends Controller {
   fire() {
     this.togglableElementTarget.classList.toggle("d-none");
     this.togglableCardTarget.classList.toggle("d-none");
+    this.mapTarget.classList.toggle("d-none");
+    this.listTarget.classList.toggle("d-none");
   }
 }
