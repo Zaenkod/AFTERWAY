3f1fcd46e4764d3d6f51016f345633899b3c0861
pierre danselme <danselmepierre@gmail.com>
2023-09-05 15:50:17 +0200

    form finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :compute_notifications_counter
-  before_action :lol
 
   private
 
@@ -9,9 +8,5 @@ class ApplicationController < ActionController::Base
     return unless current_user
 
     @notification_counter = current_user.notifications.count
-  end
-
-  def lol
-    flash[:alert] = 'oui?'
   end
 end
